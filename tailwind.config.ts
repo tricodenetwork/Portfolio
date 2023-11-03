2192b0d452566af37c85a03dce9ebccb6d4f82c7
Ovodo <ovdizzle@gmail.com>
2023-11-03 20:23:06 +0100

    Tobi joined project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "body-color": "var(--body-color)",
+        "header-color": "var(--header-color)",
+        indicatot: "var(--indicatot)",
+      },
+      fontFamily: {
+        body: "var(--body-font-family)",
+        header: "var(--header-font-family)",
+        "main-header": "var(--main-header-font-family)",
+        "sub-head": "var(--sub-head-font-family)",
+        "tiny-text": "var(--tiny-text-font-family)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
