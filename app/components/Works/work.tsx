70a72b07f92cf28e801f29e48f1ff43de68f7ef1
upraisertech <akoredesalaudeen54@gmail.com>
2023-11-11 23:20:05 +0100

    update

--- app/components/Works/work.tsx
+++ app/components/Works/work.tsx
@@ -17,8 +17,7 @@ export default function Work() {
           </div>
 
           <div className="flex-col justify-center items-center gap-[50px] flex">
-            <div className="w-full h-auto mx-3 py-8 pr-8 bg-white rounded-[10px] shadow justify-center items-center gap-4 flex">
-              <div className="w-3 h-full bg-amber-400">s</div>
+            <div className="w-full h-auto mx-3 py-8 pr-8 bg-white border border-l-amber-400 border-solid border-2 rounded-[10px] shadow justify-center items-center gap-4 flex">
               <div className="flex-col flex pl-6 gap-4">
                 <div className="self-stretch text-slate-800 text-xl md:text-[28px] font-semibold font-['Work Sans'] leading-7">
                   Unique Oil and Integrated Services Limited Lagos, Nigeria Founder/CEO 2007-Till Date
@@ -31,8 +30,7 @@ export default function Work() {
               </div>
             </div>
 
-            <div className="w-full h-auto p-8 bg-white rounded-[10px] shadow justify-center items-start gap-4 flex">
-              <div className="w-[5px] h-[500px] bg-amber-400"></div>
+            <div className="w-full h-auto py-8 pr-8 bg-white border border-l-amber-400 border-solid border-2 rounded-[10px] shadow justify-center items-start gap-4 flex">
               <div className="flex-col flex pl-6 gap-4">
                 <div className="self-stretch">
                   <span className="text-slate-800 text-[28px] font-semibold font-['Work Sans'] leading-7">NAFDAC </span>
@@ -40,6 +38,7 @@ export default function Work() {
                 </div>
                 <div className="self-stretch">
                   <span className="text-gray-600 text-xl md:text-[26px] font-normal font-['Work Sans'] leading-[30px]">
+                    <span className="w-2 h-2 bg-gray-600 rounded-full"></span>
                     Led the company(Unique Oil and Integrated Services) to forge an indomitable partnership
                     with the esteemed National Agency for Food and Drug Administration and Control
                     (The US equivalent of the FDA).<br />
@@ -61,8 +60,8 @@ export default function Work() {
 
             {works.map(({ id, title, subtitle }) => {
               return (
-                <div key={id} className="w-full h-auto py-8 pr-8 bg-white rounded-[10px] shadow justify-center items-start gap-4 flex">
-                  <div className="w-3 h-full bg-amber-400">s</div>
+                <div key={id} className="w-full h-auto py-8 pr-8 bg-white border border-l-amber-400 border-solid border-2 rounded-[10px] shadow justify-center items-start gap-4 flex">
+                  {/* <hr className="w-3 h-[300px] bg-amber-400" /> */}
                   <div className="flex-col flex pl-6 gap-4">
                     <div className="self-stretch text-slate-800  text-xl md:text-[28px] font-semibold font-['Work Sans'] leading-7">
                       {title}
