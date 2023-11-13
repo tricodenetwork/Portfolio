2d32744d66f88133ea412e0bf04fe0a250ec49d2
upraisertech <akoredesalaudeen54@gmail.com>
2023-11-13 16:39:34 +0100

    update

--- app/components/Works/work.tsx
+++ app/components/Works/work.tsx
@@ -19,31 +19,31 @@ export default function Work() {
           <div className="flex-col justify-center items-center gap-[50px] flex">
             <div className="w-full h-auto mx-3 py-8 pr-3 sm:pr-8 bg-white border border-l-amber-400 border-solid border-2 rounded-[10px] shadow justify-center items-center gap-4 flex">
               <div className="flex-col flex pl-6 gap-4">
-                <div className="self-stretch text-slate-800 text-xl md:text-[28px] font-semibold font-['Work Sans'] leading-7">
+                <div className="self-stretch text-slate-800 text-xl md:text-lg font-semibold font-['Work Sans'] leading-7">
                   Unique Oil and Integrated Services Limited Lagos, Nigeria Founder/CEO 2007-Till Date
                 </div>
-                <div className="self-stretch text-gray-600 text-xl md:text-[26px] font-normal font-['Work Sans'] leading-[30px]">Managed the Unique Oil and
+                <p className="">Managed the Unique Oil and
                   Integrated services to secure and manage critical support services for the Federal Government Agencies of the Federal Republic
                   of Nigeria (NAFDAC, FAAN, NPA), Kogi State Government, and Private companies (GAS GROUP, CITI BANK) that services multinationals
                   such as CHEVRON, AGIP, CNOO
-                </div>
+                </p>
               </div>
             </div>
 
             <div className="w-full h-auto py-8 pr-3 sm:pr-8 bg-white border border-l-amber-400 border-solid border-2 rounded-[10px] shadow justify-center items-start gap-4 flex">
               <div className="flex-col flex pl-6 gap-4">
-                <div className="self-stretch">
-                  <span className="text-slate-800  text-xl md:text-[28px] font-semibold font-['Work Sans'] leading-7">NAFDAC </span>
-                  <span className="text-slate-800  text-xl md:text-[28px] font-semibold font-['Work Sans'] leading-7">https://www.nafdac.gov.ng</span>
+                <div className="self-stretch text-slate-800 text-xl md:text-lg font-semibold">
+                  <span className="font-['Work Sans'] leading-7">NAFDAC </span>
+                  <span className="font-['Work Sans'] leading-7">https://www.nafdac.gov.ng</span>
                 </div>
-                <div className="self-stretch">
-                  <span className="text-gray-600 text-xl md:text-[26px] font-normal font-['Work Sans'] leading-[30px]">
+                <p className="self-stretch text-gray-600">
+                  <span className=" font-normal font-['Work Sans'] leading-[30px]">
                     <span className="w-2 h-2 bg-gray-600 rounded-full"></span>
                     Led the company(Unique Oil and Integrated Services) to forge an indomitable partnership
                     with the esteemed National Agency for Food and Drug Administration and Control
                     (The US equivalent of the FDA).<br />
                   </span>
-                  <span className="text-gray-600  text-xl md:text-[26px] font-normal font-['Work Sans'] leading-[30px]">
+                  <span className="text-gray-600 font-normal font-['Work Sans'] leading-[30px]">
                     Developed and implemented strategic plans to gain 70% improved energy sufficiency for operations of the
                     national laboratories of NAFDAC.
                     <br />
@@ -54,7 +54,7 @@ export default function Work() {
                     uninterrupted power supply and orchestrated efficient energy management, allowing the organization to execute its regulatory
                     functions with unparalleled effectiveness and efficiency.
                   </span>
-                </div>
+                </p>
               </div>
             </div>
 
@@ -63,12 +63,12 @@ export default function Work() {
                 <div key={id} className="w-full h-auto py-8 pr-3 sm:pr-8 bg-white border border-l-amber-400 border-solid border-2 rounded-[10px] shadow justify-center items-start gap-4 flex">
                   {/* <hr className="w-3 h-[300px] bg-amber-400" /> */}
                   <div className="flex-col flex pl-6 gap-4">
-                    <div className="self-stretch text-slate-800  text-xl md:text-[28px] font-semibold font-['Work Sans'] leading-7">
+                    <div className="self-stretch text-slate-800 text-xl md:text-lg font-semibold font-['Work Sans'] leading-7">
                       {title}
                     </div>
-                    <div className="self-stretch text-gray-600  text-xl md:text-[26px] font-normal font-['Work Sans'] leading-[30px]">
+                    <p className="self-stretch text-gray-600 font-normal font-['Work Sans'] leading-[30px]">
                       {subtitle}
-                    </div>
+                    </p>
                   </div>
                 </div>
               );
