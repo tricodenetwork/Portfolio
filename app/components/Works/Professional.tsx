ce3b599ed5b8a24b95267d6cc2e6b0673300af7d
upraisertech <akoredesalaudeen54@gmail.com>
2023-11-13 15:14:13 +0100

    volunteer_experience

--- app/components/Works/Professional.tsx
+++ app/components/Works/Professional.tsx
@@ -19,7 +19,7 @@ export default function Professional() {
         <div className="text-slate-800 text-[26px] md:text-[52px] font-semibold font-['Poppins'] leading-[56px]">Professional Experience</div>
       </div>
 
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5 mx-[2rem] justify-center items-start gap-3">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 mt-5 mx-[2rem] justify-center items-start gap-3">
         <div className="w-[90%] flex-col justify-start items-center gap-[15px]">
           <div className="w-full h-px bg-gray-600" />
           <div className="flex-col justify-start items-end gap-8 mt-3 inline-flex">
@@ -66,34 +66,37 @@ export default function Professional() {
           </div>
         </div>
 
-        <div className="w-[90%] flex-col justify-start items-center gap-[15px]">
-          <div className="w-full h-px bg-gray-600" />
-          <div className="flex-col justify-start items-start gap-8 mt-3 inline-flex">
-            <div className="text-slate-800 text-xl md:text-[20px] font-semibold font-['Work Sans'] leading-7">Exhibitor @ the 31st Council for the Regulation of Engineering in Nigeria Assembly, 2023</div>
-            <div className="h-auto">
-              <span className="text-gray-800 text-xl md:text-[16px] font-normal font-['Work Sans']">Editorial Board Member: 2023-till date </span>
-              <a href='https://journals.rcmss.com/index.php/ijpamr/about/editorialTeam'
-                className="text-gray-600 text-xl md:text-[16px] font-normal font-['Work Sans'] leading-[30px]">
-                https://journals.rcmss.com/index.php
-                /ijpamr/about/editorialTeam
-              </a>
+        <div className="w-full flex flex-col md:flex-col lg:flex-row justify-start items-start gap-12">
+          <div className="w-[90%] flex-col justify-start items-center gap-[15px]">
+            <div className="w-full h-px bg-gray-600" />
+            <div className="flex-col justify-start items-start gap-8 mt-3 inline-flex">
+              <div className="text-slate-800 text-xl md:text-[20px] font-semibold font-['Work Sans'] leading-7">Exhibitor @ the 31st Council for the Regulation of Engineering in Nigeria Assembly, 2023</div>
+              <div className="h-auto">
+                <span className="text-gray-800 text-xl md:text-[16px] font-normal font-['Work Sans']">Editorial Board Member: 2023-till date </span>
+                <a href='https://journals.rcmss.com/index.php/ijpamr/about/editorialTeam'
+                  className="text-gray-600 text-xl md:text-[16px] font-normal font-['Work Sans'] leading-[30px]">
+                  https://journals.rcmss.com/index.php
+                  /ijpamr/about/editorialTeam
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-[90%] flex-col justify-start items-center mx-3 ml-3 gap-[15px] inline-flex">
+            <div className="w-full h-px bg-gray-600" />
+            <div className="flex-col justify-start items-start gap-8 inline-flex">
+              <div className="w-full text-slate-800 text-xl md:text-[20px] font-semibold font-['Work Sans'] leading-7">Article Publication/Citations(4)</div>
+              <span className="text-gray-600 text-xl md:text-[16px] font-normal font-['Work Sans'] leading-[30px]">Editorial Board Member: 2023-till date{' '}
+                <a href='https://journals.rcmss.com/index.php/ijpamr/about/editorialTeam'
+                  className="w-[90%] text-gray-600 text-xl md:text-[16px] font-normal font-['Work Sans'] leading-[30px]">
+                  https://journals.rcmss.com/index.php
+                  /ijpamr/about/editorialTeam
+                </a>
+              </span>
             </div>
           </div>
         </div>
 
-        <div className="w-[90%] flex-col justify-start items-center gap-[15px] inline-flex">
-          <div className="w-full h-px bg-gray-600" />
-          <div className="flex-col justify-start items-start gap-8 inline-flex">
-            <div className="w-full text-slate-800 text-xl md:text-[20px] font-semibold font-['Work Sans'] leading-7">Article Publication/Citations(4)</div>
-            <span className="text-gray-600 text-xl md:text-[16px] font-normal font-['Work Sans'] leading-[30px]">Editorial Board Member: 2023-till date{' '}
-              <a href='https://journals.rcmss.com/index.php/ijpamr/about/editorialTeam'
-                className="w-[90%] text-gray-600 text-xl md:text-[16px] font-normal font-['Work Sans'] leading-[30px]">
-                https://journals.rcmss.com/index.php
-                /ijpamr/about/editorialTeam
-              </a>
-            </span>
-          </div>
-        </div>
       </div>
     </section >
   );
