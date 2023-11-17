dbd914f53b5c052953c6a2812b7c06a55b6aa482
Ovodo <ovdizzle@gmail.com>
2023-11-17 11:00:57 +0100

    Client wants a refix

--- app/page.tsx
+++ app/page.tsx
@@ -16,47 +16,45 @@ export default function Home() {
         id='hero'
         className='m-auto w-full  flex flex-col  pt-[30px] gap-3 md:flex-row  lg:h-[802px]  relative  justify-between items-center'
       >
-        <div className='space-y-10 h-[1006px] lg:h-full  lg:max-h-max flex flex-col sm:justify-between lg:justify-center  z-10 ml-[52px]'>
+        <div className='space-y-10 h-[650px] sm:h-[1006px] lg:h-full  lg:max-h-max flex flex-col justify-between lg:justify-center  z-10 ml-5 sm:ml-[52px]'>
           <h4
             style={poppins.style}
-            className=' w-full md:w-[543px] font-semibold text-header-color text-[52px] lg:text-[51px] tracking-[0] leading-[70px]'
+            className=' w-[367px] md:w-[543px] font-semibold text-header-color text-[32px] leading-[56px] lg:text-[51px] tracking-[0] lg:leading-[70px]'
           >
             Dr. Malik Abdullahi Adaviriku CMC
           </h4>
           <div className='space-y-10'>
-            <p className=" [font-family:'Poppins-Regular',Helvetica] lg:w-[645px] font-normal text-body-color text-[18px] md:text-[26px] tracking-[0] leading-[39px]">
-             <strong> Unique Group, Inc.
-              Founder CEO
-             </strong>
-              Beverly Hills, CA, USA.
-              
+            <p className=" mb-5  [font-family:'Poppins-Regular',Helvetica] lg:w-[645px] font-normal text-body-color text-[20px] leading-[28.1px] md:text-[26px] tracking-[0] md:leading-[39px]">
+              <strong> Unique Group, Inc. Founder CEO</strong> Beverly Hills,
+              CA, USA.
             </p>
-          
           </div>
         </div>
-        <div className=' sm:absolute top-[239px] right-0 lg:relative lg:right-0 lg:top-0 sm:w-[385.43px] sm:h-[561px] 2xl w-full lg:w-[482px]  lg:h-[802px]'>
+        <div className='absolute top-[180px] sm:absolute  sm:top-[239px] right-0 lg:relative lg:right-0 lg:top-0 w-[251px] h-[367px] sm:w-[385.43px] sm:h-[561px]  lg:w-[482px]  lg:h-[802px]'>
           <Image
             fill
             priority
             // width={722}
             // height={600}
             // sizes='40vw'
-            className=''
+            className='object-contain lg:object-cover'
             style={{
               // width: "100%",
               // height: "auto",
-              objectFit: "cover",
+              // objectFit: "contain",
               zIndex: "-10",
             }}
             src={Hero}
             alt='hero'
             quality={100}
           />
-          <div className='sm:w-[527px] sm:h-[527px] lg:w-[752px]  border border-blue-950 absolute bottom-0 right-[0px]  -z-20 lg:h-[752px] rounded-full bg-header-color' />
+          <div className='w-[377px] h-[377px] sm:w-[527px] sm:h-[527px] lg:w-[752px]  border border-blue-950 absolute bottom-0 right-[0px]  -z-20 lg:h-[752px] rounded-full bg-header-color' />
         </div>
       </section>
 
-      <section><HandA /> </section>
+      <section>
+        <HandA />
+      </section>
       <section>
         <AboutMe />
       </section>
@@ -67,9 +65,9 @@ export default function Home() {
         <div className='text-center'>
           <h2 className=' font-bold text-[52px] py-10'>Interest</h2>
           <p>
-            <strong>Politics Experience:</strong> 
-            Aspirant for Senate House, 2019 Federal Republic of
-            Nigeria, Community service and environmental protection programs.
+            <strong>Politics Experience:</strong>
+            Aspirant for Senate House, 2019 Federal Republic of Nigeria,
+            Community service and environmental protection programs.
           </p>
         </div>
 
